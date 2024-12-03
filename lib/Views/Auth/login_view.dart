bbd9492157153988fe8eb92052f27ec0351f2bb8
shelton15 <sheltoniyassa@gmail.com>
2024-12-03 22:02:39 +0100

    readjusted an import

--- lib/Views/Auth/login_view.dart
+++ lib/Views/Auth/login_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../Providers/auth_provider.dart';
+import '../../Providers/auth_provider.dart';
 
 
 class LoginView extends StatelessWidget {
